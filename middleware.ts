6e45bbe8026597ea9fa221339173ed697fc2c365
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-10-06 17:45:18 +0900

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,22 @@ import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest): Promise<NextResponse> {
 	if (request.method === "GET") {
-		return NextResponse.next();
+		const response = NextResponse.next();
+		const token = request.cookies.get("session")?.value ?? null;
+		if (token !== null) {
+			// Only extend cookie expiration on GET requests since we can be sure
+			// a new session wasn't set when handling the request.
+			response.cookies.set("session", token, {
+				path: "/",
+				maxAge: 60 * 60 * 24 * 30,
+				sameSite: "lax",
+				httpOnly: true,
+				secure: process.env.NODE_ENV === "production"
+			});
+		}
+		return response;
 	}
+
 	const originHeader = request.headers.get("Origin");
 	// NOTE: You may need to use `X-Forwarded-Host` instead
 	const hostHeader = request.headers.get("Host");
